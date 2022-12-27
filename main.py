6889e9be7c973aa16b804553a228ea5cae8c1803
DenisLukianov21 <DenisLukianov2001@yandex.ru>
2022-12-27 14:47:36 +0300

    rework Download + fix main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import discord
+from Download import Downloader
 from discord.ext import commands
 
 TOKEN = 'MTA1NzAwMTczNTY2ODg5MTczNw.GXZQ-A.xPpyaHbY0NBiWoJkHs2EoH9nn3nvwUDd0i2i5c'
@@ -6,6 +8,12 @@ TOKEN = 'MTA1NzAwMTczNTY2ODg5MTczNw.GXZQ-A.xPpyaHbY0NBiWoJkHs2EoH9nn3nvwUDd0i2i5
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+def delete():
+    file_dir = os.listdir('Audio')
+    for file in file_dir:
+        os.remove(f'Audio\\{file}')
 
 
 @bot.command(name='join', help='Tells the bot to join the voice channel')
@@ -23,15 +31,17 @@ async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_connected():
         await voice_client.disconnect()
+        delete()
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
 
 @bot.command(name='play_song', help='To play song')
-async def play(ctx):
+async def play(ctx, url):
     server = ctx.message.guild
     voice_channel = server.voice_client
-    source = discord.FFmpegPCMAudio(source='ЛУЧШЕЕ_HEARTHSTONE/ЧУМОВОЙ_ДИНОЗАВР)__OIlIkA.opus')
+    source = discord.FFmpegPCMAudio(
+        source=Downloader(url).download())
     async with ctx.typing():
         voice_channel.play(source)
     await ctx.send('**Now playing:** {}'.format('test song'))
